e002af82e3fc233a9bed374d15fc49c1ad855a99
zzlmilk <413124766@qq.com>
2013-12-04 14:33:24 +0800

    Initial Commit

---
+++ 008/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  008
+//
+//  Created by zzlmilk on 13-12-4.
+//  Copyright (c) 2013年 zzlmilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
